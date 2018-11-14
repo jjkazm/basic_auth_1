a0d7771de09a3f68d6b85218c5c3f3524d4334cd
jjkazm <jjkazm@gmail.com>
2018-11-14 22:42:40 +0100

    Test succesfull signing up user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  attr_accessor :password, :password_confirmation
+  before_save :hash_password
+  validates :password, confirmation: {case_sensitive: false}
+
+  def hash_password
+    if password.present?
+      self.password_hash = BCrypt::Password.create(password)
+    end
+  end
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email:"example@examlpe.com", password:"pass123", password_confirmation: "pass123")
+  end
+
+  test "should be valid" do
+    @user.save
+    assert @user.valid?
+  end
 end
