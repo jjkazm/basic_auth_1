efc041ef1b73cd3b7ca09553786616cff0fa352a
jjkazm <jjkazm@gmail.com>
2018-11-14 20:51:54 +0100

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'user/new'
+  get "signup", to: "users#new", as: "sign"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+  resources :users
+  resources :sessions
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
