7c902ecb29601c1a32b6206010b586510bc382c7
jjkazm <jjkazm@gmail.com>
2018-11-14 20:37:53 +0100

    Create actions in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+    if @user.save
+      redirect_to root_path
+      flash[:success] = "Welcome in the authorisation app. The only app where you can signup and nothing else:)"
+    else
+      render 'new'
+    end
   end
 
 end
